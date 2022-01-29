a834d2dd2f58f9d1752787bd2682da626c1b7e17
UdheySingh <56387009+UdheySingh@users.noreply.github.com>
2022-01-29 15:00:23 +0100

    Add files via upload

---
+++ http/src/main/java/com/example/resources/esempio.js
@@ -0,0 +1,7 @@
+console.clear()
+alert("ciao bello!");
+
+// const node = document.getElementById('id')
+// const node = document.getElementsByClassName('class')
+// const nodes = document.querySelectorAll('div')
+// window.addEventListener()
